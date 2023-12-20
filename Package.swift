9d77e61c1f67a80f28a1bedaa1ebaff6202d6d0c
Altaf Ur Rehman <altaf.rehman@iqeqdigital.com>
2023-12-20 15:43:30 +0500

    eMoney framework updated

--- Package.swift
+++ Package.swift
@@ -12,13 +12,14 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "eMoney",
-            targets: ["eMoney", "EFRSDK", "LeanSDK", "MDRSDK"])
+            targets: ["eMoney", "EFRSDK", "LeanSDK", "MDRSDK", "PureLiveSDK"])
     ],
     dependencies: [
         .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.0.0")),
         .package(url: "https://github.com/SwiftKickMobile/SwiftMessages.git", .upToNextMajor(from: "9.0.0")),
         .package(url: "https://github.com/airbnb/lottie-spm.git", .upToNextMajor(from: "4.3.3")),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.8.1"))
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.8.1")),
+        .package(url: "https://gitlab.com/purelive/purelive-ios-sdk.git", .upToNextMajor(from: "6.4.0.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -27,5 +28,6 @@ let package = Package(
         .binaryTarget(name: "EFRSDK", path: "./Sources/EFRSDK.xcframework"),
         .binaryTarget(name: "LeanSDK", path: "./Sources/LeanSDK.xcframework"),
         .binaryTarget(name: "MDRSDK", path: "./Sources/MDRSDK.xcframework"),
+        .binaryTarget(name: "PureLiveSDK", path: "./Sources/PureLiveSDK.xcframework")
     ]
 )
